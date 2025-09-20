c782ab49f89b1d9e2cabc0fd98ac6ceec8bbffa3
Ajex Joshy <ajexjoshyclass8@gmail.com>
2025-09-20 15:16:59 +0530

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/app/", // <-- important: makes all assets load under /app/
+  base: "/", // <-- important: makes all assets load under /app/
   plugins: [react(), tailwindcss()],
 });
--- server/src/app.js
+++ server/src/app.js
@@ -28,7 +28,7 @@ app.use(
 
 app.use("/app", express.static(path.join(__dirname, "../../client/dist/")));
 app.get("/app", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+  res.sendFile("../../client/dist/index.html");
 });
 
 app.use("/auth", authRouter);
